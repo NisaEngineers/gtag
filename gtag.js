5e385a56a4998bcb032b54cd8856a4d19dd47e40
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2026-04-16 02:41:46 +0600

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,13 @@
+This is the Code 
+
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17944460923"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17944460923');
+</script>
